a05ea408e89618b225c0885dc524cf8a30b76552
Niloo KSZ <niloofar.keshavarz@gmail.com>
2022-11-26 11:49:29 -0500

    Imports pages in App.js and index.js

--- singlepgaereact/src/App.js
+++ singlepgaereact/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Home , About, Contact} from './pages';
 
 function App() {
   return (
     <div >
-      Hello Niloofar!
+     <h1> Hello Niloofar!</h1>
+     <Home /> 
+     <About /> 
+     <Contact /> 
+
+
     </div>
   );
 }
