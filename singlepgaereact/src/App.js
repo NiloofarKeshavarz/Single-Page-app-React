b9348fa4edfc3998f4b37e22128c23fa5fbd100d
Niloo KSZ <niloofar.keshavarz@gmail.com>
2022-11-26 14:18:10 -0500

    Adds NavLink to Nav.js

--- singlepgaereact/src/App.js
+++ singlepgaereact/src/App.js
@@ -22,7 +22,7 @@ function App() {
         <Route path ="/contact">
           <Contact /> 
         </Route>
-        <Route path ="/">
+        <Route  path ="/">
           <Home /> 
         </Route>
         </Switch>
