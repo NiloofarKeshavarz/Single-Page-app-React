994a421739fca33a6e706d2ca575643c8132e79d
Niloo KSZ <niloofar.keshavarz@gmail.com>
2022-11-26 17:12:08 -0500

    Edits Components

---
+++ singlepgaereact/src/components/nav.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+const navs = [
+    {
+        path: '/',
+        name: 'Home',
+    },
+    {
+        path: '/about',
+        name: 'About',
+
+    },
+    {
+        path: '/contact',
+        name: 'Contact',
+    }
+]
+
+const nav = () =>(
+    <nav className="bg-purple-600 text-purple-300 p-4 h-16 shadow">
+            <ul className="flex space-x-4 justify-end">
+            
+           
+            {navs.map((navItem =>(
+                 <li>
+                    <NavLink 
+                    exact to ={navItem.path} activeClassName='border-b-2 border-purple-200'>{navItem.name}</NavLink></li>
+
+            )))}
+            </ul>
+        </nav>
+
+)
+export default nav;
